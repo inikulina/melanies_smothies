5298a0f8a130185814055f2e5697a27753606f99
inikulina <inikulina@shelterinsurance.com>
2024-10-27 18:28:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,10 +38,7 @@ if ingredients_list:
                values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
     
-    #st.write(my_insert_stmt)
-    #st.stop()
-    
-    time_to_insert =st.button("Submit Order")
+       time_to_insert =st.button("Submit Order")
     
     if time_to_insert:
        session.sql(my_insert_stmt).collect()
